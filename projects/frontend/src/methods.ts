ff9d1638efce777b7d79161f6235924e33b957a5
sam2much96 <sam2much96@gmail.com>
2024-12-26 07:56:32 +0100

    new file:   api/connect_wallet.py 	new file:   api/show_price.py 	new file:   api/update_listing.py 	deleted:    projects/frontend/index.html 	modified:   projects/frontend/src/methods.ts 	new file:   public/backend.py 	renamed:    projects/frontend/digitalmarketplace.html -> public/index.html

--- projects/frontend/src/methods.ts
+++ projects/frontend/src/methods.ts
@@ -1,12 +1,37 @@
 import * as algokit from "@algorandfoundation/algokit-utils"
 
-import { MarketplaceClient } from "./contracts/Marketplace"
+import { MarketplaceClient, MarketplaceParamsFactory } from "./contracts/Marketplace"
 
 
 // framework agnostic wrapper
 
-export function create(algorand: algokit.AlgorandClient, dmClient: MarketplaceClient, assetBeingSold: bigint, listingPrice: bigint) {
+export function create(algorand: algokit.AlgorandClient,
+    dmClient: MarketplaceClient,
+    assetBeingSold: bigint,
+    listingPrice: bigint,
+    quantity: bigint,
+    sender: string) {
+
     //return an async function
-    return async () => { await dmClient }
+    return async () => {
+        let assetId = assetBeingSold;
+
+        if (assetId === 0n) {
+            const assetCreate = await algorand.send.assetCreate({ sender, total: quantity });
+            assetId = BigInt(assetCreate.confirmation.assetIndex!)
+        }
+
+        const params = (MarketplaceParamsFactory.create.createApplication({ args: [assetId, listingPrice] }));
+        const createResult = await dmClient.appClient.createTransaction.call(params);
+
+        console.log("Debug 1: ", createResult);
+        let appId = "";
+
+        const mbrTxn = await algorand.createTransaction.payment({ sender, receiver: appId, amount: algokit.algos(0.1 + 0.1), extraFee: algokit.algos(0.0001) });  //createResult.appAddress
+
+        console.log("Debug 2: ", mbrTxn);
+        //await dmClient.createTransaction.optInToAsset({ mbrTxn });
+
+    }
 
 } // deploy app function
