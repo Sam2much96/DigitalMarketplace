1397b17662e9cf0098bfb80930926eecde0db6f5
sam2much96 <sam2much96@gmail.com>
2024-12-27 15:27:42 +0100

    Testnet Update

--- projects/frontend/src/methods.ts
+++ projects/frontend/src/methods.ts
@@ -1,37 +1,125 @@
-import * as algokit from "@algorandfoundation/algokit-utils"
+import { AlgorandClient, Config, microAlgos } from '@algorandfoundation/algokit-utils'
 
-import { MarketplaceClient, MarketplaceParamsFactory } from "../contracts/Marketplace.js"
+import { MarketplaceClient, MarketplaceFactory, MarketplaceParamsFactory } from "./contracts/Marketplace.ts"
+
+
+// To DO : 
+// (1) Create algorand client function
 
 
 // framework agnostic wrapper
-
-export function create(algorand: algokit.AlgorandClient,
-    dmClient: MarketplaceClient,
+// creates the digital marketplace dApp
+export function create(
+    //algorand: AlgorandClient,
+    //dmClient: MarketplaceParamsFactory,
     assetBeingSold: bigint,
     listingPrice: bigint,
     quantity: bigint,
-    sender: string) {
+    sender: string,
+    setAppId: (id: number) => void,
+) {
 
-    //return an async function
+    //function returns an async function
     return async () => {
+        console.log("Creating Asa");
+        // turn on debug
+        Config.configure({ debug: true });
+
+        const algorand = AlgorandClient.testNet(); // Algonode free tier
+        //const algorand = AlgorandClient.fromEnvironment();
+        //const deployer = algorand.account.fromEnvironment("DEPLOYER", (1).algo());
+
+        // fund creator address
+        console.log("Is Testnet: ", algorand.client.isTestNet());
+        console.log("Is Localnet: ", algorand.client.isLocalNet());
+
         let assetId = assetBeingSold;
 
         if (assetId === BigInt(0)) {
+            // create an asset if asset id is 0
+
+            console.log("Creating Asa");
             const assetCreate = await algorand.send.assetCreate({ sender, total: quantity });
-            assetId = BigInt(assetCreate.confirmation.assetIndex!)
+            assetId = BigInt(assetCreate.confirmation.assetIndex!);
+            console.log("Asset id created:", assetId);
         }
 
-        const params = (MarketplaceParamsFactory.create.createApplication({ args: [assetId, listingPrice] }));
-        const createResult = await dmClient.appClient.createTransaction.call(params);
+        // create the typed app factory
+        const factory = algorand.client.getTypedAppFactory(MarketplaceFactory, { defaultSender: sender })
 
-        console.log("Debug 1: ", createResult);
-        let appId = "";
 
-        // const mbrTxn = await algorand.createTransaction.payment({ sender, receiver: appId, amount: algokit.algos(0.1 + 0.1), extraFee: algokit.algos(0.0001) });  //createResult.appAddress
 
-        //console.log("Debug 2: ", mbrTxn);
-        //await dmClient.createTransaction.optInToAsset({ mbrTxn });
+        const params = await MarketplaceParamsFactory.create.createApplication({ args: [assetId, listingPrice] });
 
+        const { appClient } = await factory.deploy(params);
+
+        console.log(" App ID: ", appClient.appId);
+
+        // make an abi call to the client
+        const response = await appClient.params.hello({ args: { name: "world" } });
+
+        console.log("App Client Debug: ", response);
+
+
+        // create an app call from the client to create a application
+        //const createResult = await dmClient.appClient.create.createApplication({ assetId, listingPrice });
+        //const createResult = await MarketplaceParamsFactory.create.createApplication({ args: [assetId, listingPrice] });
+        // const {createResult } = await MarketplaceFactory.se  
+
+        //console.log("Debug 1: ", createResult);
+
+        //console.log("Debug 1: ", createResult.appId);
+
+
+        //const mbrTxn = await algorand.createTransaction.payment({ sender, receiver: createResult.appAddress, amount: algokit.algos(0.1 + 0.1), extraFee: algokit.algos(0.0001) });  //mbr + asset fee + inner transaction fee
+
+        //await MarketplaceClient.appClient.optInToAsset({ mbrTxn });
+
+        //await dmClient.createTransaction.optInToAsset(mbrTxn);
+
+        //await algorand.send.assetTransfer({ sender, assetId, receiver: createResult.appAddress, amount: quantity });
+
+        // update the frontend ui
+        //setAppId(Number(createResult.appId));
     }
 
-} // deploy app function
+}
+
+export function buy(
+    algorand: AlgorandClient,
+    dmClient: MarketplaceClient,
+    sender: string,
+    appAddress: string,
+    quantity: bigint,
+    listingPrice: bigint,
+    setUnitsLeft: (units: bigint) => void
+) {
+    return async () => {
+
+        // construct the buy transation
+        const buyerTxn = await algorand.createTransaction.payment({ sender, receiver: appAddress, amount: microAlgos(quantity * listingPrice) });
+
+        console.log("Debug 2: ", buyerTxn);
+        //await dmClient.appClient.buy(buyerTxn, quantity);
+
+        //get the dApp global state
+        //const state = await dmClient.appClient.getGlobalState();
+        //const info = await algorand.account.getAssetInformation(appAddress, state.assetId!.asBigInt());
+
+
+        //update the frontend ui
+        //setUnitsLeft(info.balance);
+    }
+}
+
+export function deleteApp(
+    algorand: AlgorandClient,
+    dmClient: MarketplaceClient,
+    setAppId: (id: number) => void,
+) {
+    return async () => {
+
+        //await dmClient.appClient.delete.deleteApplication({});
+        setAppId(0);
+    }
+}
--- projects/market/tests/marketplace_client_test.py
+++ projects/market/tests/marketplace_client_test.py
@@ -1,24 +1,40 @@
 import algokit_utils
+import os
 import pytest
 from algokit_utils import get_localnet_default_account, TransactionParameters
 from algokit_utils.config import config
-from algosdk.v2client.algod import AlgodClient
-from algosdk.v2client.indexer import IndexerClient
+#from algosdk.v2client.algod import AlgodClient
+#from algosdk.v2client.indexer import IndexerClient
 
 from smart_contracts.artifacts.marketplace.marketplace_client import MarketplaceClient
 
-from algokit_utils.beta.algorand_client import AlgorandClient,  PayParams, AssetCreateParams,AssetTransferParams, AssetOptInParams
+from algokit_utils.beta.algorand_client import AlgorandClient,  PayParams, AssetCreateParams,AssetTransferParams, AssetOptInParams, SuggestedParams
 from algokit_utils.beta.account_manager import AddressAndSigner
 
 import algosdk
-from algosdk.atomic_transaction_composer import TransactionWithSigner
+from algosdk.atomic_transaction_composer import TransactionWithSigner, TransactionSigner,AccountTransactionSigner
+
+from algosdk.mnemonic import to_private_key
+from algosdk.account import address_from_private_key
+#from algokit_utils.beta.account_manager import TransactionSignerAccount
+
+
+class AlgorandAccount:
+    def __init__(self, mnemonic: str):
+        self._private_key = to_private_key(mnemonic)
+        self.address = address_from_private_key(self._private_key)
+        self.signer = AccountTransactionSigner(self._private_key)
+    #def get_signer(self):
+    #    return AccountTransactionSigner(self._private_key)
+
 
 @pytest.fixture(scope="session")
 def algorand() -> AlgorandClient:
     """
     Get An Algorand Client to use throughout the test
     """
-    return AlgorandClient.default_local_net()
+    # AlgorandClient.default_localnet()
+    return AlgorandClient.test_net()
 
 
 @pytest.fixture(scope="session")
@@ -26,33 +42,36 @@ def dispenser(algorand : AlgorandClient) -> AddressAndSigner:
     """
     Get A Dispenser to fund the test address
     """
-
+    # uses environment dispenser mnemonic to fund the account for testnet
     return algorand.account.dispenser()
 
 
 @pytest.fixture(scope="session")
 def creator(algorand: AlgorandClient, dispenser: AddressAndSigner) -> AddressAndSigner:
-    """Create New Random Algorand Account for testing"""
-    acct = algorand.account.random()
+    #"""Create New Random Algorand Account for testing"""
+    # use my algorand test accout fpr testing
+
+
+    acct =  AlgorandAccount(os.environ["ACCOUNT_MNEMONIC"]) #algorand.account.random()
+    #signer = acct.get_signer()
+
+    algorand.set_default_signer( acct.signer)  # Register the creator's signer as default signer for all test txns.
+
     algorand.send.payment(PayParams(
         sender=dispenser.address,
         receiver=acct.address,
-        amount= 10_000_000
+        amount= 100_000
     ))
-
-    return acct
-
-@pytest.fixture(scope="session")
-def test_asset_id(creator : AddressAndSigner, algorand: AlgorandClient) -> int:
-    sent_txn = algorand.send.asset_create(AssetCreateParams(sender = creator.address, total=10))
-    return sent_txn["confirmation"]["asset-index"]
+    creator = AddressAndSigner(address=acct.address, signer=acct.signer)
+    return creator
+    #return acct
 
 
 @pytest.fixture(scope="session")
 def marketplace_client(
     algorand : AlgorandClient, creator : AddressAndSigner, test_asset_id : int
 ) -> MarketplaceClient:
-    """Instantiate an application client we can use for our test"""
+    #Instantiate an application client we can use for our test
     
     config.configure(
         debug=True,
@@ -65,25 +84,36 @@ def marketplace_client(
         signer = creator.signer
     )
 
-    # compile smart contract
+    # compile smart contractwith asset id
     client.create_create_application(listingPrice=0,assetId=test_asset_id)
 
     return client
 
 
+
+@pytest.fixture(scope="session")
+def test_asset_id(creator : AddressAndSigner, algorand: AlgorandClient) -> int:
+    params = algorand.get_suggested_params()#SuggestedParams
+    sent_txn = algorand.send.asset_create(AssetCreateParams(sender = creator.address, total=10, first_valid_round=params.first, last_valid_round=params.last)) # account for network delays
+    return sent_txn["confirmation"]["asset-index"]
+
+# Pytests start from here
 def test_opt_in_to_asset(marketplace_client : MarketplaceClient, creator : AddressAndSigner, test_asset_id : int, algorand : AlgorandClient):
     # throws error if app not yet oped int 
     pytest.raises(algosdk.error.AlgodHTTPError, 
                   lambda: algorand.account.get_asset_information(marketplace_client.app_address, test_asset_id)
                   )
-   
-    # Web need to send 100_000 for account mbr and 100_000 for asa mbr
+    params = algorand.get_suggested_params()#SuggestedParams
+
+    # Web need to send 100_000 for account mbr and 100_000 for asa mbr, to fund the Dapp Address
     # create an opt in transaction for the test asset
     mbr_pay_txn =  algorand.transactions.payment(PayParams(
         sender = creator.address,
         receiver = marketplace_client.app_address,
         amount = 200_000,
-        extra_fee = 1_000
+        first_valid_round=params.first,
+        last_valid_round=params.last,
+        extra_fee = 1_000,
 
     ))
 
@@ -103,42 +133,51 @@ def test_opt_in_to_asset(marketplace_client : MarketplaceClient, creator : Addre
 
 
 def test_deposit(marketplace_client : MarketplaceClient, creator : AddressAndSigner, test_asset_id : int, algorand : AlgorandClient):
+    params = algorand.get_suggested_params()#SuggestedParams
+    
     result = algorand.send.asset_transfer(AssetTransferParams(
         sender=creator.address,
         asset_id=test_asset_id,
         receiver=marketplace_client.app_address,
-        amount = 3
+        amount = 3,
+        first_valid_round=params.first,
+        last_valid_round=params.last
         ))
     
     assert result["confirmation"]
     assert (algorand.account.get_asset_information(marketplace_client.app_address, test_asset_id)["asset-holding"]["amount"] == 3)
 
 
+
 def test_set_price(marketplace_client : MarketplaceClient):
-    result = marketplace_client.set_price(listingPrice=3_300_000)
+    result = marketplace_client.set_price(listingPrice=300_000) # set price to 3  algos
 
     assert result.confirmed_round
 
 
 
 def test_buy(marketplace_client : MarketplaceClient, creator : AddressAndSigner, test_asset_id : int, algorand : AlgorandClient, dispenser : AddressAndSigner):
-    
+    params = algorand.get_suggested_params()#SuggestedParams
     # create new account to be the buyer
     buyer = algorand.account.random()
 
-    # use dispenser to fund buyer
-    algorand.send.payment(PayParams(sender= dispenser.address, receiver= buyer.address, amount = 10_000_000 ))
+    # use dispenser to fund buyer for buy transaction
+    algorand.send.payment(PayParams(sender= dispenser.address, receiver= buyer.address, amount = 603_000, first_valid_round=params.first, last_valid_round=params.last ))
 
     # opt buyer into the asset
-    algorand.send.asset_opt_in(AssetOptInParams( sender= buyer.address, asset_id=test_asset_id))
+    algorand.send.asset_opt_in(AssetOptInParams( sender= buyer.address, asset_id=test_asset_id, first_valid_round=params.first, last_valid_round=params.last))
 
-    # form a transaciton to buy 2 assets
+    assert  (algorand.get_account_balance(buyer.address) >= 600_000) # make sure buyer has enough algos to cover this
+
+    # form a transaciton to buy 2 assets from the buyer address
     buyer_payment_txn = algorand.transactions.payment(
         PayParams(
             sender = buyer.address,
             receiver= marketplace_client.app_address,
-            amount= 2 * 3_300_000,
-            extra_fee= 1_000 # double extra fee for every inner transaction in the smart contract
+            amount= 2 * 300_000,
+            extra_fee= 1_000, # double extra fee for every inner transaction in the smart contract
+            first_valid_round=params.first, 
+            last_valid_round=params.last
         )
     )
 
@@ -152,14 +191,15 @@ def test_buy(marketplace_client : MarketplaceClient, creator : AddressAndSigner,
     assert (algorand.account.get_asset_information(buyer.address, test_asset_id)["asset-holding"]["amount"] == 2)
 
 
-
-def test_says_hello(marketplace_client: MarketplaceClient) -> None:
+def test_say_hello(marketplace_client: MarketplaceClient) -> None:
     result = marketplace_client.hello(name="World")
 
     assert result.return_value == "Hello,World"
 
+
+"""
 def test_simulate_says_hello_with_correct_budget_consumed(
-    marketplace_client: MarketplaceClient, algod_client: AlgodClient
+    marketplace_client: MarketplaceClient, algod_client: AlgorandClient,
 ) -> None:
     result = (
         marketplace_client.compose().hello(name="World").hello(name="Jane").simulate()
@@ -170,8 +210,10 @@ def test_simulate_says_hello_with_correct_budget_consumed(
     assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
 
 
-
+"""
+@pytest.fixture(scope="session")
 def test_delete_application(marketplace_client : MarketplaceClient, creator : AddressAndSigner, test_asset_id : int, algorand : AlgorandClient, dispenser : AddressAndSigner):
+    # would fail if dApp does not have 1000 micro algos to cover the inner transaction fees via overspend logical error
     before_call_amount = algorand.account.get_information(creator.address)["amount"]
 
     result = marketplace_client.delete_delete_application(
@@ -184,4 +226,3 @@ def test_delete_application(marketplace_client : MarketplaceClient, creator : Ad
     after_call_amount = algorand.account.get_information(creator.address)["amount"]
     assert after_call_amount - before_call_amount == (2 * 3_300_000) + 200_000 - 3000 # 
     assert(algorand.account.get_asset_information(creator.address, test_asset_id)["asset-holding"]["amount"] == 8)
-
